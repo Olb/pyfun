7a20096d95f717f0ce2f65e9567f3007a67bda23
billy <braybilly@gmail.com>
2018-05-13 13:56:44 +0300

    Test

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+
+class PyFun(unittest.TestCase):
+  def setUp(self):
+    print("Test 1")
+
+  def test_prints(sef):
+    print("Test 2)
+
+
+if __name__ == "__main__":
+  unittest.main()
